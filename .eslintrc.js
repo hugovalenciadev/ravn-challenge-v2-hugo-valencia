3bd892c4d6262c8541bfebec23c8fec6a9ece60f
Hugo Valencia <iam@hugovalencia.dev>
2023-01-23 10:39:30 -0600

    feat: customize eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,21 +5,36 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'jest', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:eslint-comments/recommended',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
+  },  
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'error',
+    'max-len': ['error', { code: 150, tabWidth: 2, ignoreUrls: true, ignoreComments: true }],
+    semi: 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [['builtin', 'external'], 'parent', 'sibling', 'index'],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+    'import/newline-after-import': ['error', { count: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    curly: ['error'],
   },
 };
