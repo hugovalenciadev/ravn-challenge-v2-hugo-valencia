c6e0afba442427a7ec08da9f0b3c42c406a44e58
Hugo Valencia <iam@hugovalencia.dev>
2023-01-24 13:22:02 -0600

    feat: add enabled property as part of the update input

--- src/products/dtos/update-product.dto.ts
+++ src/products/dtos/update-product.dto.ts
@@ -1,4 +1,9 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
+import { IsBoolean, IsOptional } from 'class-validator';
 import { CreateProductDto } from './create-product.dto';
 
-export class UpdateProductDto extends PartialType(OmitType(CreateProductDto, ['categories'] as const)) {}
+export class UpdateProductDto extends PartialType(OmitType(CreateProductDto, ['categories'] as const)) {
+  @IsBoolean()
+  @IsOptional()
+  enabled?: boolean;
+}
